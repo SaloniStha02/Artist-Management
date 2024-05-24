b52c6108e9caa2e68ffc27edc6d4069ee31beac0
SaloniStha02 <saloni.stha2002@gmail.com>
2024-05-24 16:22:36 +0545

    custom color

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    colors:{
+      'blackblue':'#1E0F75',
+      'darkblue':'#1C1DAB',
+      'lightblue':'#3785D8',
+      'blue':'#ADC6E5',
+      'purple':'#afa0ec',
+      'pink':'#e15ff8',
+      'green':'#306c79',
+      'peach':'#EBB2C3',
+      'grey':'#CBD8E8',
+      'white':'#ffffff',
+      'black':'#000000',
+    }
+  },
+  plugins: [],
+}
